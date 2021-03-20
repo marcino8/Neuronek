0d295a43d0181c1518ddb8917ecb27d6bd78a81a
klaudia <klaudiapajor17@gmail.com>
2021-03-20 12:35:03 +0100

    ..

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ class Neuron:
         if len(self.dendrons) == 0:
             return
         for dendron in self.dendrons:
-            sumOutput = sumOutput + dendron.connectedNeuron.getOutput() * dendron.weight
+            sumOutput = sumOutput + float(dendron.connectedNeuron.getOutput()) * dendron.weight
         self.output = self.sigmoid(sumOutput)
 
     def backPropagate(self):
@@ -217,37 +217,35 @@ class Network:
         output.pop()  # removing the bias neuron
         return output
 
-def main():
-    topology = []
-    topology.append(2)
-    topology.append(3)
-    topology.append(2)
-    net = Network(topology)
-    Neuron.eta = 0.09
-    Neuron.alpha = 0.015
-    inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    outputs = [[0, 0], [1, 0], [1, 0], [0, 1]]
-    while True:
-        err = 0
-        for i in range(len(inputs)):
-            net.setInput(inputs[i])
-            net.feedForword()
-            net.backPropagate(outputs[i])
-            err = err + net.getError(outputs[i])
-        print("error: ", err)
-        if err < 0.01:
-            break
 
-    while True:
-        a = input("type 1st input :")
-        b = input("type 2nd input :")
-        net.setInput([a, b])
+topology = []
+topology.append(2)
+topology.append(3)
+topology.append(2)
+net = Network(topology)
+Neuron.eta = 0.09
+Neuron.alpha = 0.015
+inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+outputs = [[0, 0], [1, 0], [1, 0], [0, 1]]
+while True:
+    err = 0
+    for i in range(len(inputs)):
+        net.setInput(inputs[i])
         net.feedForword()
-        print(net.getThResults())
+        net.backPropagate(outputs[i])
+        err = err + net.getError(outputs[i])
+    print("error: ", err)
+    if err < 0.91:
+        break
+
+while True:
+    a = input("type 1st input :")
+    b = input("type 2nd input :")
+    net.setInput([a, b])
+    net.feedForword()
+    print(net.getThResults())
 
 
-if __name__ == '__main__':
-    main()
 
 
 
